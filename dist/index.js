fcab8aea6751735c9cf61005193eaf60a38a665c
Mathias Walz <mathias.walz@sage.de>
2019-01-16 11:31:25 +0100

    Modification flag

--- dist/index.js
+++ dist/index.js
@@ -4,14 +4,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { observable, computed } from 'mobx';
+import { observable, computed, isObservable } from 'mobx';
 import { render } from './render';
+const field = observable;
 function extendComplexTypeMetadata(baseTypeMetadata, derrivedTypeMetadata) {
     let properties = {};
-    if (baseTypeMetadata.properties) {
+    if (baseTypeMetadata && baseTypeMetadata.properties) {
         properties = Object.assign({}, baseTypeMetadata.properties);
     }
-    if (derrivedTypeMetadata.properties) {
+    if (derrivedTypeMetadata && derrivedTypeMetadata.properties) {
         properties = Object.assign({}, properties, derrivedTypeMetadata.properties);
     }
     return {
@@ -20,19 +21,28 @@ function extendComplexTypeMetadata(baseTypeMetadata, derrivedTypeMetadata) {
     };
 }
 class StateModelNode {
-    constructor($parent) {
-        this.$parent = $parent;
-    }
 }
-class ValueField extends StateModelNode {
+class Field extends StateModelNode {
+}
+class ValueField extends Field {
+    setValue(value) {
+        let oldValue = this.value;
+        this.value = value;
+        if (value !== oldValue) {
+            this.fireOnValueChanged(value, oldValue);
+        }
+    }
+    fireOnValueChanged(value, oldValue) {
+    }
 }
 __decorate([
     observable
 ], ValueField.prototype, "value", void 0);
 class TextField extends ValueField {
 }
-class ComplexType {
+class ComplexType extends StateModelNode {
     constructor() {
+        super();
         let metadata = this.getMetadata();
         if (!metadata) {
             throw new Error("A complex type needs to defina a static property metadata of type IComplexTypeMetadata: " + this.constructor.name);
@@ -41,27 +51,40 @@ class ComplexType {
     getMetadata() {
         return this.constructor.metaData;
     }
-    applyMetadata() {
-        console.log(this, this.getMetadata());
+    init() {
+        this.initFields();
+        this.initStaticMetadata();
+    }
+    initStaticMetadata() {
+    }
+    initFields() {
+        Object.getOwnPropertyNames(this).forEach((propertyName) => {
+            let propertyValue = this[propertyName];
+            if (isObservable(propertyValue)) {
+                propertyValue.fieldName = propertyName;
+                propertyValue.$parent = this;
+            }
+        });
     }
 }
-class ComplexField extends StateModelNode {
-    constructor(parent, _itemCtor) {
-        super(parent);
+class ComplexField extends Field {
+    constructor(_itemCtor) {
+        super();
         this._itemCtor = _itemCtor;
         this.value = new _itemCtor();
-        this.value.applyMetadata();
+        this.value.$parent = this;
+        this.value.init();
     }
     getItemMetadata() {
         return this._itemCtor.metaData;
     }
 }
 __decorate([
-    observable
+    field
 ], ComplexField.prototype, "value", void 0);
-class ArrayField extends StateModelNode {
-    constructor($parent, _itemCtor) {
-        super($parent);
+class ArrayField extends Field {
+    constructor(_itemCtor) {
+        super();
         this._itemCtor = _itemCtor;
         this._items = [];
     }
@@ -71,7 +94,8 @@ class ArrayField extends StateModelNode {
     append(item) {
         if (item == null) {
             item = new this._itemCtor();
-            item.applyMetadata();
+            item.$parent = this;
+            item.init();
         }
         this._items.push(item);
         return item;
@@ -81,7 +105,7 @@ class ArrayField extends StateModelNode {
     }
 }
 __decorate([
-    observable
+    field
 ], ArrayField.prototype, "_items", void 0);
 __decorate([
     computed
@@ -89,19 +113,19 @@ __decorate([
 class Address extends ComplexType {
     constructor() {
         super(...arguments);
-        this.line1 = new TextField(this);
+        this.line1 = new TextField();
     }
 }
 Address.metaData = {
     name: "Address"
 };
 __decorate([
-    observable
+    field
 ], Address.prototype, "line1", void 0);
 class ExtendedAddress extends Address {
     constructor() {
         super(...arguments);
-        this.line2 = new TextField(this);
+        this.line2 = new TextField();
     }
 }
 ExtendedAddress.metaData = extendComplexTypeMetadata(Address.metaData, {
@@ -113,14 +137,14 @@ ExtendedAddress.metaData = extendComplexTypeMetadata(Address.metaData, {
     }
 });
 __decorate([
-    observable
+    field
 ], ExtendedAddress.prototype, "line2", void 0);
 class Customer extends ComplexType {
     constructor() {
         super(...arguments);
-        this.name = new TextField(this);
-        this.address = new ComplexField(this, Address);
-        this.addresses = new ArrayField(this, Address);
+        this.name = new TextField();
+        this.address = new ComplexField(Address);
+        this.addresses = new ArrayField(Address);
     }
 }
 Customer.metaData = {
@@ -130,49 +154,49 @@ Customer.metaData = {
     }
 };
 __decorate([
-    observable
+    field
 ], Customer.prototype, "name", void 0);
 __decorate([
-    observable
+    field
 ], Customer.prototype, "address", void 0);
 __decorate([
-    observable
+    field
 ], Customer.prototype, "addresses", void 0);
 class SpecialCustomer extends Customer {
     constructor() {
         super(...arguments);
-        this.matchcode = new TextField(this);
-        this.address = new ComplexField(this, ExtendedAddress);
-        this.addresses = new ArrayField(this, ExtendedAddress);
+        this.matchcode = new TextField();
+        this.address = new ComplexField(ExtendedAddress);
+        this.addresses = new ArrayField(ExtendedAddress);
     }
 }
 SpecialCustomer.metaData = extendComplexTypeMetadata(Customer.metaData, {
     name: "SpecialCustomer"
 });
 __decorate([
-    observable
+    field
 ], SpecialCustomer.prototype, "matchcode", void 0);
 __decorate([
-    observable
+    field
 ], SpecialCustomer.prototype, "address", void 0);
 __decorate([
-    observable
+    field
 ], SpecialCustomer.prototype, "addresses", void 0);
 class ListDetailViewModel extends ComplexType {
     constructor(listCtor, detailCtor) {
         super();
-        this.detail = new ComplexField(this, detailCtor);
-        this.items = new ArrayField(this, listCtor);
+        this.detail = new ComplexField(detailCtor);
+        this.items = new ArrayField(listCtor);
     }
 }
 ListDetailViewModel.metaData = {
     name: "ListDetailViewModel"
 };
 __decorate([
-    observable
+    field
 ], ListDetailViewModel.prototype, "items", void 0);
 __decorate([
-    observable
+    field
 ], ListDetailViewModel.prototype, "detail", void 0);
 class ListDetailView {
     constructor(listCtor, detailCtor) {
@@ -181,6 +205,7 @@ class ListDetailView {
 }
 new ListDetailView(Customer, SpecialCustomer);
 const customer = new SpecialCustomer();
+customer.init();
 let secs = 0;
 setInterval(() => {
     secs++;
@@ -196,17 +221,10 @@ setInterval(() => {
     if (num > 0) {
         customer.addresses.items[num - 1].line1.value = "done";
     }
-    if (num % 2 == 0) {
-        let a = new ExtendedAddress();
-        a.line1.value = "number: " + num;
-        customer.addresses.append(a);
-    }
-    else {
-        let a = customer.addresses.append();
-        a.line1.value = "number: " + num;
-    }
+    let a = customer.addresses.append();
+    a.line1.value = "number: " + num;
+    a.line2.setValue(a.line1.value);
+    let x;
 }, 3000);
-console.log(customer.getMetadata());
-console.log(customer.address.getItemMetadata());
-console.log(customer.addresses.getItemsMetadata());
+console.log(customer);
 render(customer);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 
-import { observable, computed } from 'mobx';
+import { observable, computed, isObservable, autorun } from 'mobx';
 import { render } from './render';
 
 type ReadonlyArrayInnerType<T> = T extends ReadonlyArray<(infer U)> ? U : never;
 type ArrayFieldInnerType<T> = T extends IArrayField<(infer U)> ? U : never;
 
+const field = observable;
 interface IFieldMetadata {
 
 }
@@ -17,12 +18,12 @@ interface IComplexTypeMetadata {
 
 function extendComplexTypeMetadata(baseTypeMetadata: IComplexTypeMetadata, derrivedTypeMetadata: IComplexTypeMetadata): IComplexTypeMetadata {
     let properties = {};
-    if (baseTypeMetadata.properties) {
+    if (baseTypeMetadata && baseTypeMetadata.properties) {
         properties = {
             ...baseTypeMetadata.properties
         };
     }
-    if (derrivedTypeMetadata.properties) {
+    if (derrivedTypeMetadata && derrivedTypeMetadata.properties) {
         properties = {
             ...properties,
             ...derrivedTypeMetadata.properties
@@ -35,36 +36,58 @@ function extendComplexTypeMetadata(baseTypeMetadata: IComplexTypeMetadata, derri
 }
 
 interface IStateModelNode {
-
+    readonly $parent: IStateModelNode;
 }
 class StateModelNode implements IStateModelNode {
-    constructor(protected $parent: IStateModelNode) {
-    }
+    readonly $parent: IStateModelNode;
 }
-type FieldType = {
-
+interface IField extends IStateModelNode {
+    readonly fieldName: string;
+    readonly modified: boolean;
+    updateSnapshot();
 }
-interface IField extends FieldType {
-
+abstract class Field extends StateModelNode implements IField {
+    public fieldName: string;
+    abstract readonly modified: boolean;
+    abstract updateSnapshot();
 }
 interface IValueField<TValue> extends IField {
     value: TValue;
+    snapshotValue: TValue;
+    setValue(value: TValue);
 }
-
-class ValueField<TValue> extends StateModelNode implements IValueField<TValue> {
+class ValueField<TValue> extends Field implements IValueField<TValue> {
     @observable value: TValue;
+    @observable snapshotValue: TValue;
+    public setValue(value: TValue) {
+        let oldValue = this.value;
+        this.value = value;
+        if (value !== oldValue) {
+            this.fireOnValueChanged(value, oldValue);
+        }
+    }
+    @computed get modified(): boolean {
+        return this.value !== this.snapshotValue;
+    }
+    protected fireOnValueChanged(value, oldValue) {
+    }
+    public updateSnapshot() {
+        this.snapshotValue = this.value;
+    }
 }
 
 interface ITextField extends IValueField<string> {
 }
 
 class TextField extends ValueField<string> implements ITextField {
-
 }
-interface IComplexType {
+interface IComplexType extends IStateModelNode {
+    readonly modified: boolean;
+    updateSnapshot();
 }
-class ComplexType implements IComplexType {
+class ComplexType extends StateModelNode implements IComplexType {
     constructor() {
+        super();
         let metadata = this.getMetadata();
         if (!metadata) {
             throw new Error("A complex type needs to defina a static property metadata of type IComplexTypeMetadata: " + this.constructor.name);
@@ -73,8 +96,43 @@ class ComplexType implements IComplexType {
     getMetadata(): IComplexTypeMetadata {
         return (this.constructor as any).metaData as IComplexTypeMetadata;
     }
-    applyMetadata() {
-        console.log(this, this.getMetadata());
+    public init() {
+        this.initFields();
+        this.initStaticMetadata();
+    }
+    protected initStaticMetadata() {
+    }
+    protected initFields() {
+        let fieldNames = this.getFieldNames();
+        fieldNames.forEach((propertyName) => {
+            let propertyValue = this[propertyName];
+            propertyValue.fieldName = propertyName;
+            propertyValue.$parent = this;
+        });
+    }
+    protected getFieldNames(): string[] {
+        let fieldNames = (this as any).$gwFieldNames;
+        if (fieldNames) {
+            return fieldNames;
+        }
+        fieldNames = (this as any).$gwFieldNames = [];
+        Object.getOwnPropertyNames(this).forEach((propertyName) => {
+            let propertyValue = this[propertyName];
+            if (propertyName.charAt(0) !== "$" && isObservable(propertyValue)) {
+                fieldNames.push(propertyName);
+            }
+        });
+        return fieldNames;
+    }
+    @computed public get modified(): boolean {
+        return this.getFieldNames().some((fieldName => {
+            return this[fieldName].modified;
+        }));
+    }
+    public updateSnapshot() {
+        return this.getFieldNames().some((fieldName => {
+            return this[fieldName].updateSnapshot();
+        }));
     }
 }
 
@@ -83,16 +141,23 @@ interface IComplexField<TComplexType extends IComplexType> extends IField {
     getItemMetadata(): IComplexTypeMetadata;
 }
 
-class ComplexField<TComplexType extends IComplexType> extends StateModelNode implements IComplexField<TComplexType> {
-    constructor(parent: IStateModelNode, private _itemCtor: new () => TComplexType) {
-        super(parent);
+class ComplexField<TComplexType extends IComplexType> extends Field implements IComplexField<TComplexType> {
+    constructor(private _itemCtor: new () => TComplexType) {
+        super();
         this.value = new _itemCtor();
-        (this.value as any).applyMetadata();
+        (this.value as any).$parent = this;
+        (this.value as any).init();
     }
     public getItemMetadata(): IComplexTypeMetadata {
         return (this._itemCtor as any).metaData as IComplexTypeMetadata;
     }
-    @observable readonly value: TComplexType;
+    updateSnapshot() {
+        this.value.updateSnapshot();
+    }
+    @computed public get modified(): boolean {
+        return this.value.modified;
+    }
+    @field readonly value: TComplexType;
 }
 
 interface IArrayField<TComplexType extends IComplexType> extends IField {
@@ -101,24 +166,29 @@ interface IArrayField<TComplexType extends IComplexType> extends IField {
     append(item?: TComplexType): TComplexType;
 }
 
-class ArrayField<TComplexType extends IComplexType> extends StateModelNode implements IArrayField<TComplexType> {
-    constructor($parent: IStateModelNode, private _itemCtor: new () => TComplexType) {
-        super($parent);
+class ArrayField<TComplexType extends IComplexType> extends Field implements IArrayField<TComplexType> {
+    constructor(private _itemCtor: new () => TComplexType) {
+        super();
     }
-    @observable private _items = [];
+    @field private _items = [];
     @computed get items(): ReadonlyArray<TComplexType> {
         return this._items;
     }
     public append(item?: TComplexType): TComplexType {
         if (item == null) {
-            item = new this._itemCtor();
-            (item as any).applyMetadata();
+            let ctor = this._itemCtor;
+            item = new ctor();
+            (item as any).$parent = this;
+            (item as any).init();
         }
         this._items.push(item);
         return item;
     }
     public getItemsMetadata(): IComplexTypeMetadata {
         return (this._itemCtor as any).metaData as IComplexTypeMetadata;
+    }
+    modified: boolean;
+    updateSnapshot() {
     }
 }
 
@@ -130,7 +200,7 @@ class Address extends ComplexType implements IAddress {
     static metaData = {
         name: "Address"
     };
-    @observable line1: ITextField = new TextField(this);
+    @field line1: ITextField = new TextField();
 }
 
 interface IExtendedAddress extends IAddress {
@@ -148,7 +218,7 @@ class ExtendedAddress extends Address implements IExtendedAddress {
         }
     })
 
-    @observable readonly line2 = new TextField(this);
+    @field readonly line2 = new TextField();
 }
 
 interface ICustomer extends ComplexType {
@@ -164,9 +234,9 @@ class Customer extends ComplexType implements ICustomer {
             name
         }
     };
-    @observable name: ITextField = new TextField(this);
-    @observable address: IComplexField<IAddress> = new ComplexField(this, Address);
-    @observable addresses: IArrayField<IAddress> = new ArrayField(this, Address);
+    @field name: ITextField = new TextField();
+    @field address: IComplexField<IAddress> = new ComplexField(Address);
+    @field addresses: IArrayField<IAddress> = new ArrayField(Address);
 }
 
 interface ISpecialCustomer extends ICustomer {
@@ -174,13 +244,14 @@ interface ISpecialCustomer extends ICustomer {
     readonly address: IComplexField<IExtendedAddress>;
     readonly addresses: IArrayField<IExtendedAddress>;
 }
+
 class SpecialCustomer extends Customer implements ISpecialCustomer {
     static metaData: IComplexTypeMetadata = extendComplexTypeMetadata(Customer.metaData, {
         name: "SpecialCustomer"
     });
-    @observable readonly matchcode: ITextField = new TextField(this);
-    @observable readonly address: IComplexField<IExtendedAddress> = new ComplexField(this, ExtendedAddress);
-    @observable readonly addresses: IArrayField<IExtendedAddress> = new ArrayField(this, ExtendedAddress);
+    @field readonly matchcode: ITextField = new TextField();
+    @field readonly address: IComplexField<IExtendedAddress> = new ComplexField(ExtendedAddress);
+    @field readonly addresses: IArrayField<IExtendedAddress> = new ArrayField(ExtendedAddress);
 }
 
 class ListDetailViewModel<TListType extends IComplexType, TDetailType extends IComplexType> extends ComplexType {
@@ -190,11 +261,11 @@ class ListDetailViewModel<TListType extends IComplexType, TDetailType extends IC
 
     constructor(listCtor: new () => TListType, detailCtor: new () => TDetailType) {
         super();
-        this.detail = new ComplexField(this, detailCtor);
-        this.items = new ArrayField<TListType>(this, listCtor);
+        this.detail = new ComplexField(detailCtor);
+        this.items = new ArrayField<TListType>(listCtor);
     }
-    @observable items: IArrayField<TListType>;
-    @observable detail: IComplexField<TDetailType>;
+    @field items: IArrayField<TListType>;
+    @field detail: IComplexField<TDetailType>;
 }
 
 class ListDetailView<TL extends IComplexType, TD extends IComplexType> {
@@ -208,7 +279,15 @@ class ListDetailView<TL extends IComplexType, TD extends IComplexType> {
 new ListDetailView(Customer, SpecialCustomer);
 
 const customer = new SpecialCustomer();
-//console.log(customer);
+customer.init();
+customer.updateSnapshot();
+autorun(() => {
+    console.log(customer.modified);
+});
+
+customer.address.value.line1.value = "hi";
+customer.address.value.line1.value = undefined;
+
 let secs = 0;
 setInterval(() => {
     secs++;
@@ -227,20 +306,14 @@ setInterval(() => {
     if (num > 0) {
         customer.addresses.items[num - 1].line1.value = "done";
     }
-    if (num % 2 == 0) {
-        let a = new ExtendedAddress();
-        a.line1.value = "number: " + num;
-        customer.addresses.append(a);
-    } else {
-        let a = customer.addresses.append();
-        a.line1.value = "number: " + num;
-    }
+    let a = customer.addresses.append();
+    a.line1.value = "number: " + num;
+    a.line2.setValue(a.line1.value);
+
     type a = ReadonlyArrayInnerType<typeof customer.addresses.items>;
     type b = ArrayFieldInnerType<typeof customer.addresses>;
     let x: b;
 }, 3000)
-console.log(customer.getMetadata())
-console.log(customer.address.getItemMetadata());
-console.log(customer.addresses.getItemsMetadata());
 
+console.log(customer);
 render(customer);
